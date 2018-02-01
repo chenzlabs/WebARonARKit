fd33aafa6e9c29caed26cfcad2eb929070c230e2
M C <machenmusik@gmail.com>
2018-02-01 14:01:59 -0500

    avoid adding image anchor twice to event list

--- WebARonARKit/WebARonARKit.js
+++ WebARonARKit/WebARonARKit.js
@@ -1141,7 +1141,6 @@
             newAnchor.name = eventAnchor.name;
             newAnchor.physicalSize = eventAnchor.physicalSize;
             WebARonARKitVRDisplay.anchors_.set(eventAnchor.identifier, newAnchor);
-            updatedAnchors.push(newAnchor);
           }
           var anchor = WebARonARKitVRDisplay.anchors_.get(
               eventAnchor.identifier);
