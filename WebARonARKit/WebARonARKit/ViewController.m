1b33eb05b3e333354dc604d29c5512fe82276601
M C <machenmusik@gmail.com>
2018-04-13 18:56:30 -0400

    allow inline media playback, without user gesture

--- WebARonARKit/WebARonARKit/ViewController.m
+++ WebARonARKit/WebARonARKit/ViewController.m
@@ -354,6 +354,10 @@ const float CAMERA_FRAME_JPEG_COMPRESSION_FACTOR = 0.5;
     WKWebViewConfiguration *wkWebViewConfig =
     [[WKWebViewConfiguration alloc] init];
     wkWebViewConfig.userContentController = userContentController;
+    
+    wkWebViewConfig.allowsInlineMediaPlayback = true;
+    wkWebViewConfig.mediaTypesRequiringUserActionForPlayback = WKAudiovisualMediaTypeNone;
+    
     // Create the WKWebView using the configuration/script injection and add it to
     // the top of the view graph
     wkWebView = [[WKWebView alloc] initWithFrame:self.view.frame
@@ -880,6 +884,7 @@ const float CAMERA_FRAME_JPEG_COMPRESSION_FACTOR = 0.5;
                               stringWithFormat:
                               @"\"%@\"",
                               anchor.identifier.UUIDString];
+                NSLog(@"imageAnchor: %@", additionalStr);
             }
         }
         NSString *anchorStr = [NSString
